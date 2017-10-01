bb5bfd004a8e9d42e3d5697698a5960d46466f5e
deobieta <deobieta@gmail.com>
2017-10-01 17:50:17 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,27 @@ output "lambda_function_s3_arn" {
 output "lambda_function_s3_vpc_arn" {
   value = "${aws_lambda_function.s3_vpc.arn}"
 }
+
+output "lambda_function_s3_vpc_dl_arn" {
+  value = "${aws_lambda_function.s3_vpc_dl.arn}"
+}
+
+output "lambda_function_s3_dl_arn" {
+  value = "${aws_lambda_function.s3_dl.arn}"
+}
+
+output "lambda_function_arn" {
+  value = "${aws_lambda_function.s3.arn}"
+}
+
+output "lambda_function_filename_vpc_arn" {
+  value = "${aws_lambda_function.filename_vpc.arn}"
+}
+
+output "lambda_function_filename_vpc_dl_arn" {
+  value = "${aws_lambda_function.filename_vpc_dl.arn}"
+}
+
+output "lambda_function_filename_dl_arn" {
+  value = "${aws_lambda_function.filename_dl.arn}"
+}
